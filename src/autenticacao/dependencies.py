7dc3b3e350e81f9054e90c9cc24873026e797331
YvesPereira21 <yvespereira85@gmail.com>
2026-01-02 21:58:21 -0300

    feat: implementa funcoes de gerar token de acesso e refresh

---
+++ src/autenticacao/dependencies.py
@@ -0,0 +1,94 @@
+from typing import Any, List, Optional
+from typing_extensions import Unpack
+from fastapi import Request, Depends, status
+from fastapi.exceptions import HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from .utils import decode_token
+from sqlmodel.ext.asyncio.session import AsyncSession
+from src.db.main import get_session
+from .service import UserService
+from src.db.models import User
+
+
+user_service = UserService()
+
+class TokenBearer(HTTPBearer):
+    def __init__(self, auto_error=True):
+        return super().__init__(auto_error=auto_error)
+
+
+    async def __call__(self, request: Request) -> Optional[HTTPAuthorizationCredentials]:
+        creds = await super().__call__(request)
+        token = creds.credentials
+
+        token_data = decode_token(token)
+
+        return token_data
+
+
+    def token_valid(self, token: str) -> bool:
+        token_data = decode_token(token)
+
+        return True if token_data is not None else False
+
+
+    def verify_token_data(self, token_data):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Please override this method in child classes"
+        )
+
+
+class AccessTokenBearer(TokenBearer):
+    def verify_access_token(self, token_data: dict) -> None:
+        if not token_data:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Invalid token"
+            )
+        
+        if token_data['refresh']:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Access token required"
+            )
+
+
+class RefreshTokenBearer(TokenBearer):
+    def veriverify_access_token(self, token_data: dict) -> None:
+        if not token_data:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Invalid token"
+            )
+
+        if not token_data['refresh']:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Refresh token required"
+            )
+
+
+async def get_current_user(
+        token_data: dict,
+        session: AsyncSession = Depends(get_session)
+):
+    user_email = token_data['user']['email']
+    user = await user_service.get_user_by_email(user_email, session)
+
+    return user
+
+
+class RoleChecker:
+    def __init__(self, allowed_roles: List[str]) -> None:
+        self.allowed_roles = allowed_roles
+
+
+    def __call__(self, current_user: User = Depends(get_current_user)) -> Any:
+        if current_user.role in self.allowed_roles:
+            return True
+
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="You don't have access to this"
+        )
---
+++ src/autenticacao/utils.py
@@ -0,0 +1,52 @@
+import uuid
+import jwt
+import logging
+from datetime import datetime, timedelta
+from src.config import Config
+from passlib.context import CryptContext
+
+
+password_context = CryptContext(schemes=['bcrypt'])
+
+ACESS_TOKEN_EXPIRE = 3600
+
+def generate_password_hash(password: str) -> str:
+    hash = password_context.hash(password)
+
+    return hash
+
+
+def verify_password(password: str, hash: str) -> bool:
+    return password_context.verify(password, hash)
+
+
+def create_access_token(user_data: dict, expire: timedelta = None, refresh: bool = False):
+    payload = {}
+
+    payload['user'] = user_data
+    payload['exp'] = datetime.now() + (expire if expire is not None else ACESS_TOKEN_EXPIRE)
+    payload['jti'] = str(uuid.uuid4())
+    payload['refresh'] = refresh
+
+    token = jwt.encode(
+        payload=payload,
+        key=Config.JWT_SECRET,
+        algorithm=Config.JWT_ALGORITHM
+    )
+
+    return token
+
+
+def decode_token(token: str) -> dict:
+    try:
+        token_decoded = jwt.decode(
+            jwt=token,
+            key=Config.JWT_SECRET,
+            algorithms=[Config.JWT_ALGORITHM]
+        )
+
+        return token_decoded
+    except jwt.PyJWTError as error:
+        logging.error(error)
+
+        return {}
